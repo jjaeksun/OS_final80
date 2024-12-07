a93e92402217c4b305b8fa9323e0165ef935d0b6
JuWon Kang <your-email-rkdwndnjs0918@naver.com>
2024-12-07 10:58:35 +0900

    app predict plus

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
 from transformers import ViTFeatureExtractor, ViTForImageClassification
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 app = Flask(__name__)
-
 
 model = ViTForImageClassification.from_pretrained("nateraw/vit-age-classifier")
 feature_extractor = ViTFeatureExtractor.from_pretrained("nateraw/vit-age-classifier")
@@ -12,8 +11,8 @@ feature_extractor = ViTFeatureExtractor.from_pretrained("nateraw/vit-age-classif
 def upload_file():
     return '''
     <!doctype html>
-    <title>Age Prediction</title>
-    <h1>Upload an image to predict age</h1>
+    <title>연령 예측하기</title>
+    <h1>예측 할 이미지를 선택하세요</h1>
     <form method="POST" action="/predict" enctype="multipart/form-data">
       <input type="file" name="file">
       <input type="submit" value="Upload">
@@ -22,17 +21,22 @@ def upload_file():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    file = request.files["file"]
-    image = Image.open(file.stream)
+    try:
+        
+        file = request.files["file"]
+        
+       
+        image = Image.open(file.stream)
+    except UnidentifiedImageError:
+       
+        return "<h1>Error: Uploaded file is not a valid image. Please try again with a valid image file.</h1>", 400
 
-    
+   
     inputs = feature_extractor(images=image, return_tensors="pt")
-
-
     outputs = model(**inputs)
     predicted_class = outputs.logits.argmax(-1).item()
 
-   
+    
     return f"<h1>Predicted Age Group: {predicted_class}</h1>"
 
 if __name__ == "__main__":
