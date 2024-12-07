4769395bc5ca53aba852523219312fe6b7c7296f
namu-sky <namu684@gachon.ac.kr>
2024-12-08 01:34:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 model = ViTForImageClassification.from_pretrained("nateraw/vit-age-classifier")
 feature_extractor = ViTFeatureExtractor.from_pretrained("nateraw/vit-age-classifier")
 
+age_categories = {
+    0: (0, 10),
+    1: (11, 20),
+    2: (21, 30),
+    3: (31, 40),
+    4: (41, 50),
+    5: (51, 60),
+    6: (61, 70),
+    7: (71, 80),
+    8: (81, 90),
+    9: (91, 100)
+}
+
 @app.route("/")
 def upload_file():
     return '''
@@ -34,10 +47,12 @@ def predict():
    
     inputs = feature_extractor(images=image, return_tensors="pt")
     outputs = model(**inputs)
-    predicted_class = outputs.logits.argmax(-1).item()
+    logits = outputs.logits
+    predicted_class = logits.argmax(-1).item()
 
+    predicted_age_range = age_categories.get(predicted_class, "Unknown Age Range")
     
-    return f"<h1>예측되는 나이: {predicted_class}</h1>"
+    return f"<h1>예측되는 나이: {predicted_age_range[0]} - {predicted_age_range[1]}세</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
