c70794ba112d901dfdb9ee9ff681f2d7a58a98b4
sheehun <sheehun87@gachon.ac.kr>
2024-12-11 12:35:36 +0900

    Update app.py
    결과 화면이 글자만 나와서 result.html과 연동해 조금 더 깔끔한 페이지가 나오도록 파이썬 코드를 수정했습니다.

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from PIL import Image, UnidentifiedImageError
 
 app = Flask(__name__)
 
+# 모델 및 특성 추출기 로드
 model = ViTForImageClassification.from_pretrained("nateraw/vit-age-classifier")
 feature_extractor = ViTFeatureExtractor.from_pretrained("nateraw/vit-age-classifier")
 
+# 연령 카테고리 매핑
 age_categories = {
     0: (0, 10),
     1: (11, 20),
@@ -22,37 +24,24 @@ age_categories = {
 
 @app.route("/")
 def upload_file():
-    return '''
-    <!doctype html>
-    <title>연령 예측하기</title>
-    <h1>예측 할 이미지를 선택하세요</h1>
-    <form method="POST" action="/predict" enctype="multipart/form-data">
-      <input type="file" name="file">
-      <input type="submit" value="Upload">
-    </form>
-    '''
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
-        
         file = request.files["file"]
-        
-       
         image = Image.open(file.stream)
     except UnidentifiedImageError:
-       
-        return "<h1>Error: Uploaded file is not a valid image. Please try again with a valid image file.</h1>", 400
+        return "<h1>유효하지 않은 이미지 파일입니다. 다시 업로드하세요.</h1>", 400
 
-   
     inputs = feature_extractor(images=image, return_tensors="pt")
     outputs = model(**inputs)
     logits = outputs.logits
     predicted_class = logits.argmax(-1).item()
+    predicted_age_range = age_categories.get(predicted_class, "알 수 없는 나이 범위")
+    predicted_age = f"{predicted_age_range[0]} - {predicted_age_range[1]}"
 
-    predicted_age_range = age_categories.get(predicted_class, "Unknown Age Range")
-    
-    return f"<h1>예측되는 나이: {predicted_age_range[0]} - {predicted_age_range[1]}세</h1>"
+    return render_template("result.html", predicted_age=predicted_age)
 
 if __name__ == "__main__":
     app.run(debug=True)
